f3d7a32b92d8326644f2797e10de76836097af64
Roman SHCH <shchevelev_r@bk.ru>
2023-04-09 16:33:11 +0300

    Example005_if_or_else

---
+++ Example005_if_or_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.Equals("Херило с горы", StringComparison.InvariantCultureIgnoreCase))
+{
+    Console.WriteLine("Ура, это же Херило с горы!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
